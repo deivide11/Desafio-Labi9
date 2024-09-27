aa35add57df17a85655af739d42eabf638a632c4
Deivide Hernique <deivideh03.com@gmail.com>
2024-09-27 02:00:36 -0300

    Terminando lista de categoria

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -31,8 +31,8 @@ const Main = () => {
                     },
                 })
                 .then(() => {
-                    Notiflix.Notify.success("Excluido com Sucesso!");
                     window.location.reload();
+                    Notiflix.Notify.success("Excluido com Sucesso!");
                 })
                 .catch((error) => {
                     console.log(error)
@@ -47,39 +47,45 @@ const Main = () => {
         <Sidebar/>
             <div className='mae-list'>
                 <div className='c-list-prod'>
-                    <div className=''>
-                        <div>
-                            <h3>Lista de Produtos</h3>
-                            <Link to="/product/create">Novo Produto</Link>
+                    <div className='transp'>
+                        <div className='h-list'>
+                            <h3 className='name-list'>Lista de Produtos</h3>
+                            <div>
+                                <input  className='search'></input>
+                            </div>
+                            <div>
+                                <Link to="/product/create" className='btn-new'>Novo Produto</Link>
+                            </div>
                         </div>
-                        <div className='name'>
-                            <h3>ID</h3>
-                            <h3>Produto</h3>
-                            <h3>Preço</h3>
-                            <h3>Ações</h3>
-                        </div>
-                        {product &&
-                            product.map(pro => (
-                                <div className="item" key={pro.id}>
-                                    <div>
-                                        <h2> {pro.id} </h2>
-                                    </div>
-                                    <div>
-                                        <h2> {pro.name} </h2>
-                                    </div>
-                                    <div>
-                                        <h2>R${pro.price} </h2>
-                                    </div>
-                                    <div>
+
+                        <div className='miolo'>
+                            <div className='name'>
+                                <h3>ID</h3>
+                                <h3>Produto</h3>
+                                <h3>Preço</h3>
+                                <h3>Ações</h3>
+                            </div>
+                            {product &&
+                                product.map(pro => (
+                                    <div className="item" key={pro.id}>
                                         <div>
-                                            <Link to={"/products/edit/" + pro.id} className="edit btn-cursor">Editar</Link>
-                                            <Link onClick={() => Remove(pro.id)} className="delete btn-cursor">Excluir</Link>
-                                            <Link to={"/products/detail/" + pro.id} >Detalhes</Link>
+                                            <h2> {pro.id} </h2>
+                                        </div>
+                                        <div>
+                                            <h2> {pro.name} </h2>
+                                        </div>
+                                        <div>
+                                            <h2>R${pro.price} </h2>
+                                        </div>
+                                        <div className='c-btn'>
+                                                <Link to={"/products/edit/" + pro.id} className="edit btn-cursor">Editar</Link>
+                                                <Link onClick={() => Remove(pro.id)} className="delete btn-cursor">Excluir</Link>
+                                                <Link to={"/products/detail/" + pro.id} className='detail btn-cursor' >Detalhes</Link>
                                         </div>
                                     </div>
-                                </div>
-                            ))
-                        }
+                                ))
+                            }
+                        </div>
                     </div>
                 </div>
             </div>
